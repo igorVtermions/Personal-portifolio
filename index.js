34d8a50443f687621c4d01eb5f82d80c9679c13a
Igor Vinicius <igorviniciusf10@gmail.com>
2023-05-08 21:44:01 -0300

    adjust tecnology e apresentation

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const sidebar = document.querySelector('nav')
 const toggle = document.querySelector('.toggle')
-const btnApre = document.querySelector('.btnApresentation')
-const ModalApre = document.querySelector('.modalApresentation')
 const closeApre = document.querySelector('.bx-x')
 const modalContact = document.querySelector('.modalContact')
 const closeContact = document.querySelector('#x')
@@ -10,19 +8,6 @@ const btnContact = document.querySelector('.btnContact')
 toggle.addEventListener('click', ()=>{
     sidebar.classList.toggle("close");
 })
-
-function openModalApre (){
-    ModalApre.style.display = 'block'
-}
-
-btnApre.addEventListener('click', openModalApre)
-
-function closeModalApre(){
-    ModalApre.style.display = 'none'
-}
-
-closeApre.addEventListener('click', closeModalApre)
-
 
 function openModalContact(){
     modalContact.style.display = 'flex'
